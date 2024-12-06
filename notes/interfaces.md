2c255695c08b4d045c71a37e4786b1f108c45c24
Samuel Leal (Valtech.com) <samuel.leal@valtech.com>
2024-12-06 16:52:06 -0300

    Interface updates

--- notes/interfaces.md
+++ notes/interfaces.md
@@ -5,3 +5,12 @@
   - You can implement an interface in a class to enforce the structure of the class.
   - Interfaces can be extended to create new interfaces. This is useful when you want to add new properties to an existing interface.
   - Differently from Classes, you can extend multiple Interfaces at once.
+
+# Summary of TypeScript Features
+
+- **Interfaces**: Used to define the structure of an object. They can be extended and implemented by classes.
+- **Optional Properties**: Properties that may or may not be present in an interface.
+- **Readonly Properties**: Properties that cannot be modified after they are set.
+- **Intersection Types**: Combine multiple types into one, resulting in a type that has all the properties of the intersected types.
+- **Union Types**: Define a type that can be one of several types.
+- **Function Types**: Interfaces can also be used to define the structure of functions.
--- topics/interfaces.ts
+++ topics/interfaces.ts
@@ -3,10 +3,19 @@
 // interface Named {
 //   readonly name: string;
 // }
+// let nameInt: Named = { name: "Sam" };
+// // nameInt.name = "João"; // This will throw an error because name is a readonly property
 
 // // age is an optional property
 // interface Age {
 //   age?: number;
+// }
+// // let ageInt: Age = { };
+
+// interface Address {
+//   street: string;
+//   city: string;
+//   postalCode: string;
 // }
 
 // interface Greetable extends Named, Age {
@@ -16,12 +25,17 @@
 // // This is a class that implements the Greetable interface. It would be possible to implement multiple interfaces by separating them with a comma
 // class Person implements Greetable {
 //   name: string;
-//   age?: number;
-//   constructor(n: string, a?: number) {
+//   age: number;
+//   // street: string;
+//   // city: string;
+//   // postalCode: string;
+
+//   constructor(n: string, a: number) {
 //     this.name = n;
-//     if (a) {
-//       this.age = a;
-//     }
+//     this.age = a;
+//     // this.street = s;
+//     // this.city = c;
+//     // this.postalCode = p;
 //   }
 
 //   greet(phrase: string): void {
@@ -29,24 +43,38 @@
 //   }
 // }
 
+// class AnotherPerson {
+//   name: string;
+
+//   constructor(n: string) {
+//     this.name = n;
+//   }
+// }
+
 // let user1: Greetable;
 
-// user1 = new Person("Sam");
-// // user1.name = "João"; // This will throw an error because name is a readonly property
+// user1 = {
+//   name: "Sam",
+//   age: 30,
+//   street: "Rua X",
+// }
 
-// // user1.greet("Hello " + user1.name + '! Happy ' + user1.age + 'th birthday!');
+// user1 = new AnotherPerson("Sam"); // This will throw an error because AnotherPerson does not implement the Greetable interface
+// user1.name = "João"; // This will throw an error because name is a readonly property
+
+// user1.greet("Hello " + user1.name + '! Happy ' + user1.age + 'th birthday!');
 
 // console.log(user1);
 
 // // Interface can be used to define functions also
-// // interface AddFn {
-// //   (a: number, b: number): number;
-// // }
-// // let add: AddFn;
-// // add = (n1: number, n2: number) => {
-// //   return n1 + n2;
-// // };
-// // console.log(add(2, 3));
+// interface AddFn {
+//   (a: number, b: number): number;
+// }
+// let add: AddFn;
+// add = (n1: number, n2: number) => {
+//   return n1 + n2;
+// };
+// console.log(add(2, 3));
 
 
 // // ###### Intersection Types
